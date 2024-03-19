bf2ab4985ae0b5a234787076e4d3ff01ce4b8f5b
matzy <matzy@matzys-MBP.lan>
2024-03-20 08:43:35 +1000

    Added tenant client

--- cmd/main.go
+++ cmd/main.go
@@ -2,36 +2,70 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 
 	//    "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
-	"context"
-
+	// "context"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
 )
 
 const subscriptionID = "0be01d84-8432-4558-9aba-ecd204a3ee61"
 
 func main() {
-	fmt.Println("Fetching subscription", subscriptionID)
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		// TODO: handle
-		fmt.Println("Auth error")
-	} else {
-		fmt.Println(&cred)
-	}
+	fmt.Println("Fetching Azure Resources")
+	// cred, err := azidentity.NewDefaultAzureCredential(nil)
+	// if err != nil {
+	// 	// TODO: handle
+	// 	fmt.Println("Auth error")
+	// } else {
+	// 	fmt.Println(&cred)
+	// }
 
-	client, err := armsubscription.NewSubscriptionsClient(cred, nil)
-	if err != nil {
-		// TODO: Handle Error
-		fmt.Println("New Subscription client error")
-	}
+	client, _ := GetAuthz()
+	fmt.Println(client)
 
-	_, err = client.Get(context.TODO(), subscriptionID, nil)
-	if err != nil {
-		// TODO: handle error
-		fmt.Println("Subscription error")
-	}
 }
+
+func GetAuthz() (*azidentity.DefaultAzureCredential, error) {
+
+	client, err := azidentity.NewDefaultAzureCredential(nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("Authentication to Azure failed with: %s", err)
+	}
+	return client, nil
+}
+
+func GetTenantClient() armsubscription.TenantsClient {
+	cred, _ := GetAuthz()
+	// var result armsubscription.TenantIDDescription
+	client, err := armsubscription.NewTenantsClient(cred, nil)
+	if err != nil {
+		log.Fatalf("Failed to fetch tenant information: %s", err)
+	}
+	return *client
+}
+
+func GetTenants() armsubscription.TenantListResult {
+	var result armsubscription.TenantListResult
+
+	return result
+}
+
+func GetTenantSubscriptions() {
+
+}
+
+// client, err := armsubscription.NewSubscriptionsClient(cred, nil)
+// if err != nil {
+// 	// TODO: Handle Error
+// 	fmt.Println("New Subscription client error")
+// }
+
+// _, err = client.Get(context.TODO(), subscriptionID, nil)
+// if err != nil {
+// 	// TODO: handle error
+// 	fmt.Println("Subscription error")
+// }
